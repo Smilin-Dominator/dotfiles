90d14021388f346dfd642df7f6623ee2abae4fcd
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-09-23 23:34:12 +0530

    feat(nvim): Made bufferline show all buffers

--- nvim/.config/nvim/after/plugin/bufferline.rc.lua
+++ nvim/.config/nvim/after/plugin/bufferline.rc.lua
@@ -3,7 +3,7 @@ if (not status) then return end
 
 bufferline.setup({
   options = {
-    mode = "tabs",
+    mode = 'buffers',
     separator_style = 'slant',
     always_show_bufferline = true,
     show_buffer_close_icons = false,
@@ -12,21 +12,14 @@ bufferline.setup({
   },
   highlights = {
     separator = {
-      fg = '#073642',
+      fg = '#1b1c36',
       bg = '#002b36',
     },
     separator_selected = {
-      fg = '#073642',
-    },
-    background = {
-      fg = '#657b83',
-      bg = '#002b36'
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
+      fg = '#1b1c36',
     },
     fill = {
-      bg = '#073642'
+      bg = '#1b1c36'
     }
   },
 })
