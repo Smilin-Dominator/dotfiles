e488cbb2d9667c488d9d346df04372c1bdf8511e
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2022-11-23 22:51:40 +0530

    feat(script): Updated the setup script

--- setup.sh
+++ setup.sh
@@ -4,15 +4,13 @@ stow git
 stow zsh
 stow alacritty 
 stow btop
-stow i3
 stow neofetch
 stow nitrogen
 stow picom
 stow rofi
-stow volumeicon
 
-# Bundle the ZSH Plugins
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install Packer for nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
