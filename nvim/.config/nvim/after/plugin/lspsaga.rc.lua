abe0ae9dbce4717434ba388ded4ba60e74ad48ee
Devisha Padmaperuma <devisha.padmaperuma@gmail.com>
2023-12-24 15:55:01 +1100

    refactor(lspsaga): changed keybindings for lspsaga

--- nvim/.config/nvim/after/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/after/plugin/lspsaga.rc.lua
@@ -7,8 +7,7 @@ local keymap = vim.keymap.set;
 local opts = { noremap = true, silent = true }
 
 -- Keymaps
-keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
-keymap("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
-keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-keymap("n", "<A-d>", "<cmd>Lspsaga open_floaterm<CR>", opts)
+keymap("n","na", "<cmd>Lspsaga code_action<CR>", opts)
+keymap("n", "nr", "<cmd>Lspsaga rename<CR>", opts)
+keymap("n", "nd", "<cmd>Lspsaga hover_doc<CR>", opts)
+keymap("n", "nt", "<cmd>Lspsaga term_toggle<CR>", opts)
